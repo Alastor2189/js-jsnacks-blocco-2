2d0f4d9ee0da6f4b5378a4770dfba779b992915b
Alastor2189 <g.lucafarci89@gmail.com>
2022-04-21 00:46:01 +0200

    inserimento commenti

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,6 +1,12 @@
+// Generatore di "nomi cognomi" casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+
+// inserimento array di nomi e cognomi
 let nameUser = ['Gianluca', 'Napoleone', 'Adolf', 'Vladimir', 'Alessandro', 'Winston'];
 let surnameUser = ['Farci', 'Bonaparte', 'Hitler', 'Putin', 'Magno', 'Churchill'];
 
+
+// selezione casuale di 3 nomi e 3 cognomi
 
 const falseGuest = [];
 for (let i = 0; i < 3; i++) {
